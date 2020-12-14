754bce30839f9bc3c79e1b1c0919827e32e2c1a0
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-12-14 19:46:16 -0300

    posicionamento global funcionando funcionando

--- src/main/java/com/example/diegoteixeira/checkinlocais/Controller/MainActivity.java
+++ src/main/java/com/example/diegoteixeira/checkinlocais/Controller/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.diegoteixeira.checkinlocais.Controller;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -25,13 +31,18 @@ import com.example.diegoteixeira.checkinlocais.Util.BancoDadosSingleton;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, LocationListener {
-    public LocationListener lm;
-    public Criteria criteria;
-    public String provider;
-
     public ArrayAdapter<String> adaptador;
     public ArrayList<String> locaisVisitados = new ArrayList<>();
     public ArrayList<String> categoria = new ArrayList<>();
+
+    public LocationManager lm;
+    public Criteria criteria;
+    public String provider;
+    public int TEMPO_REQUISICAO_LATLONG = 5000;
+    public int DISTANCIA_MIN_METROS = 0;
+
+    private final int LOCATION_PERMISSION = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +52,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // busca locais visitados no BD e popula arrayList para exibir no autoCompleteView
         Cursor c = BancoDadosSingleton.getInstance().buscar("Checkin", new String[]{"Local"}, "", "");
 
-        while(c.moveToNext()){
+        while (c.moveToNext()) {
             int L = c.getColumnIndex("Local");
             locaisVisitados.add(c.getString(L));
         }
 
         c.close();
 
-        adaptador = new ArrayAdapter<String>(this,android.R.layout.select_dialog_item, locaisVisitados);
+        adaptador = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, locaisVisitados);
         AutoCompleteTextView Local = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
         Local.setAdapter(adaptador);
 
@@ -57,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // busca categorias no BD e popula arrayList para exibir no autoCompleteView
         c = BancoDadosSingleton.getInstance().buscar("Categoria", new String[]{"nome"}, "", "");
 
-        while(c.moveToNext()){
+        while (c.moveToNext()) {
             int nome = c.getColumnIndex("nome");
             categoria.add(c.getString(nome));
         }
@@ -78,6 +89,55 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         adaptador.setNotifyOnChange(true);
         // Notifica o Spinner de que houve mudanca no modelo
         adaptador.notifyDataSetChanged();
+
+        //Location Manager
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+
+        //Testa se o aparelho tem GPS
+        PackageManager packageManager = getPackageManager();
+        boolean hasGPS = packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+
+        //Estabelece critério de precisão
+        if (hasGPS) {
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            Log.i("LOCATION", "Usando GPS");
+        } else {
+            Log.i("LOCATION", "Usando WI-FI ou dados");
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        }
+    }
+
+    public void onStart() {
+        super.onStart();
+
+        //Obtem melhor provedor habilitado com o critério estabelecido
+        provider = lm.getBestProvider(criteria, true);
+
+        if (provider == null) {
+            Log.e("PROVEDOR", "Nenhum provedor encontrado!");
+        } else {
+            Log.i("PROVEDOR", "Está sendo utilizado o provedor: " + provider);
+
+            //Obtem atualizações de posição
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                        LOCATION_PERMISSION);
+                Log.i("Permission", "Pede a permissão");
+                return;
+            }
+            lm.requestLocationUpdates(provider, TEMPO_REQUISICAO_LATLONG, DISTANCIA_MIN_METROS, this);
+        }
+    }
+
+    @Override
+    public  void onDestroy() {
+        //interrompe o Location Manager
+        lm.removeUpdates((android.location.LocationListener) this);
+        Log.w("PROVEDOR","Provedor " + provider + " parado!");
+
+        super.onDestroy();
     }
 
 
@@ -127,7 +187,35 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
+        // obtem atributos na localização atual
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
 
+        Toast.makeText(this, "latitude: "+latitude+"longitude: "+longitude, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i("Permission", "Deu a permissão");
+            } else {
+                Log.i("Permission", "Não permitiu");
+            }
+            return;
+        }
+    }
+
+    public void onProviderDisabled(String provider){
+        Log.d("LOCATION", "Desabilitou o provedor");
+    }
+
+    public void onProviderEnabled(String provider){
+        Log.d("LOCATION", "Habilitou o provedor");
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras){
+        Log.d("LOCATION", "Provedor mudou de estação");
     }
 }
 
