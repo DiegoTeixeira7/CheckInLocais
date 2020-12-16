4711dfd21836808d7d5b23be3e56e9a3f11a4cf1
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-12-16 13:58:30 -0300

    deletando locais visitados funcionando

--- src/main/java/com/example/diegoteixeira/checkinlocais/Controller/GestaoCheckin.java
+++ src/main/java/com/example/diegoteixeira/checkinlocais/Controller/GestaoCheckin.java
@@ -1,7 +1,9 @@
 package com.example.diegoteixeira.checkinlocais.Controller;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
@@ -40,7 +42,7 @@ public class GestaoCheckin extends AppCompatActivity {
                 int local = c.getColumnIndex("Local");
 
                 TextView text = new TextView(this);
-                text.setPadding(0,27,0,20);
+                text.setPadding(0,27,0,25);
                 //é obrigatório o layout_width e layout_height
                 text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                         LinearLayout.LayoutParams.WRAP_CONTENT));
@@ -67,7 +69,28 @@ public class GestaoCheckin extends AppCompatActivity {
     }
 
     private void deletarC(View v) {
-        Toast.makeText(this, "CLiquei", Toast.LENGTH_LONG).show();
+        final String tag = v.getTag().toString();
+
+        AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+        alerta.setTitle("Exclusão");
+        alerta.setMessage("Tem certeza que deseja excluir " + tag + "?");
+
+        // Configura Método executado se escolher Sim
+        alerta.setPositiveButton("SIM", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                BancoDadosSingleton.getInstance().deletar("Checkin", "Local='"+tag+"'");
+                recreate();
+            }
+        });
+
+        // Configura Método executado se escolher Não
+        alerta.setNegativeButton("NÃO", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+
+        alerta.show();
     }
 
     @Override
