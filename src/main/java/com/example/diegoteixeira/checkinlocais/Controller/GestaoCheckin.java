668fe54deb27df01d11b77d5a9c91ec1c6158c2d
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-12-16 11:38:24 -0300

    buscas no bd e botoes dinamicos adicionados na tela 3

--- src/main/java/com/example/diegoteixeira/checkinlocais/Controller/GestaoCheckin.java
+++ src/main/java/com/example/diegoteixeira/checkinlocais/Controller/GestaoCheckin.java
@@ -2,13 +2,22 @@ package com.example.diegoteixeira.checkinlocais.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.diegoteixeira.checkinlocais.R;
+import com.example.diegoteixeira.checkinlocais.Util.BancoDadosSingleton;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static java.lang.Double.parseDouble;
 
 public class GestaoCheckin extends AppCompatActivity {
     public static final int VOLTAR = 1;
@@ -20,15 +29,45 @@ public class GestaoCheckin extends AppCompatActivity {
         setTitle("GestaoCheckin");
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.layoutConteudo);
+        LinearLayout layout2 = (LinearLayout) findViewById(R.id.layoutDeletar);
 
-        for (int i = 0; i < 100; i++) {
-            TextView text = new TextView(this);
-            //é obrigatório o layout_width e layout_height
-            text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT));
-            text.setText("Texto: " + i);
-            layout.addView(text);
+        Cursor c = BancoDadosSingleton.getInstance().buscar("Checkin",
+                new String[]{"Local"}, "", "Local");
+
+        if(c.getCount() > 0) {
+
+            while (c.moveToNext()) {
+                int local = c.getColumnIndex("Local");
+
+                TextView text = new TextView(this);
+                text.setPadding(0,27,0,20);
+                //é obrigatório o layout_width e layout_height
+                text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT));
+                text.setText(c.getString(local));
+                layout.addView(text);
+
+                ImageButton btn = new ImageButton(this);
+                btn.setTag(c.getString(local));
+                btn.setImageResource(R.drawable.ic_baseline_delete_forever_24);
+                btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT));
+
+
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        deletarC(v);
+                    }
+                });
+                layout.setVerticalGravity(4);
+                layout2.addView(btn);
+            }
         }
+    }
+
+    private void deletarC(View v) {
+        Toast.makeText(this, "CLiquei", Toast.LENGTH_LONG).show();
     }
 
     @Override
