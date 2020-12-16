fdd1652760fc37234aebb5b747f258331f91a072
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-12-16 14:18:40 -0300

    relatorio finalizado

--- src/main/java/com/example/diegoteixeira/checkinlocais/Controller/Relatorio.java
+++ src/main/java/com/example/diegoteixeira/checkinlocais/Controller/Relatorio.java
@@ -2,13 +2,17 @@ package com.example.diegoteixeira.checkinlocais.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.diegoteixeira.checkinlocais.R;
+import com.example.diegoteixeira.checkinlocais.Util.BancoDadosSingleton;
 
 public class Relatorio extends AppCompatActivity {
     public static final int VOLTAR = 1;
@@ -20,14 +24,33 @@ public class Relatorio extends AppCompatActivity {
         setTitle("Relatorio");
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.layoutConteudo);
+        LinearLayout layout2 = (LinearLayout) findViewById(R.id.layoutVisitas);
 
-        for (int i = 0; i < 100; i++) {
-            TextView text = new TextView(this);
-            //é obrigatório o layout_width e layout_height
-            text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT));
-            text.setText("Texto: " + i);
-            layout.addView(text);
+        Cursor c = BancoDadosSingleton.getInstance().buscar("Checkin",
+                new String[]{"Local", "qtdVisitas"}, "", "qtdVisitas desc,Local");
+
+        if(c.getCount() > 0) {
+
+            while (c.moveToNext()) {
+                int local = c.getColumnIndex("Local");
+                int qtdV = c.getColumnIndex("qtdVisitas");
+
+                TextView text = new TextView(this);
+                text.setPadding(0,27,0,25);
+                //é obrigatório o layout_width e layout_height
+                text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT));
+                text.setText(c.getString(local));
+                layout.addView(text);
+
+                TextView text2 = new TextView(this);
+                text2.setPadding(0,27,0,25);
+                //é obrigatório o layout_width e layout_height
+                text2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT));
+                text2.setText(String.valueOf(c.getInt(qtdV)));
+                layout2.addView(text2);
+            }
         }
     }
 
